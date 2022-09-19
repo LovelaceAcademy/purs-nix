8d0392a151989f3820e148b840afadf17eca2ea4
Walker <walker@praiseweb.com.br>
2022-09-19 09:29:17 -0300

    Update CTL rev

--- namespaces/lovelaceAcademy.nix
+++ namespaces/lovelaceAcademy.nix
@@ -2,7 +2,7 @@
   cardano-transaction-lib = {
     src.git = {
       repo = "https://github.com/LovelaceAcademy/cardano-transaction-lib.git";
-      rev = "9d159e5ad132285792297eb23ad6c6b9bd781f90";
+      rev = "c89e5de2fd429f7a56d281c851a2f63f9e997979";
     };
     info = /package.nix;
   };
